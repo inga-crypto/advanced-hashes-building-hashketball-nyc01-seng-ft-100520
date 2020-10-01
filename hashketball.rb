f4832820179047f3fb6375ff1064eee816bfc02d
Inga <ijaanimets@gmail.com>
2020-10-01 22:05:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,15 +9,15 @@ def game_hash
       :colors => ['Black', 'White'],
       :players => [
         {
-          :player_name => '',
+          :player_name => 'Alan Anderson',
           :number => 0,
-          :shoe => '',
-          :points => 0,
-          :rebounds => 0,
-          :assists => 0,
-          :steals => 0,
-          :blocks => 0,
-          :slam_dunks => 0
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
         },
         # {}
       ]
