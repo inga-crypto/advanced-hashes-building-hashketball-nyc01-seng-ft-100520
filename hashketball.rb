edae1a19a55f4ec51fb92e7e15bed885ff4fcc7e
Inga <ijaanimets@gmail.com>
2020-10-01 22:09:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -81,7 +81,7 @@ def game_hash
           :slam_dunks => 0
         },
         {
-          :player_name => 'Reggie Evans',
+          :player_name => 'Bismack Biyombo',
           :number => 0,
           :shoe => 0,
           :points => 0,
