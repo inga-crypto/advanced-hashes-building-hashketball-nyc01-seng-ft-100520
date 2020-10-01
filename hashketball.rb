330467222b622a4907c49111a9674bedd09f9c3f
Inga <ijaanimets@gmail.com>
2020-10-01 22:08:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -34,16 +34,44 @@ def game_hash
           :player_name => 'Brook Lopez',
           :number => 0,
           :shoe => 0,
-          :points => 22,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 1
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
         },
         {
           :player_name => 'Mason Plumlee',
           :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
+        {
+          :player_name => 'Jason Terry',
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        }
+      ]
+    },
+    :away => {
+      :team_name => 'Brooklyn Nets',
+      :colors => ['Black', 'White'],
+      :players => [
+        {
+          :player_name => 'Alan Anderson',
+          :number => 0,
           :shoe => 16,
           :points => 22,
           :rebounds => 12,
@@ -53,20 +81,50 @@ def game_hash
           :slam_dunks => 1
         },
         {
-          :player_name => 'Jason Terry',
-          :number => 0,
-          :shoe => 16,
-          :points => 22,
+          :player_name => 'Reggie Evans',
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
           :rebounds => 12,
           :assists => 12,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 1
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7
+        },
+        {
+          :player_name => 'Brook Lopez',
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
+        {
+          :player_name => 'Mason Plumlee',
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
+        {
+          :player_name => 'Jason Terry',
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
         }
       ]
-    },
-    :away => {
-      
     }
   }
 end
