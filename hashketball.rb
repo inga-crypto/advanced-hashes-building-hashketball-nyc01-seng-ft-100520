3d397f99f191ecd669004bbff9bd41a99a8a9018
Inga <ijaanimets@gmail.com>
2020-10-01 22:03:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,11 @@ def game_hash
   team = { team_name: '', colors: ['red'], players: [player] }
   {
     :home => {
-      
+      :team_name => 'Brooklyn Nets',
+      :colors => ['Black', 'White'],
+      :players => [
+        { :player_name => '', :number => 0, :shoe => '', :points
+      ]
     },
     :away => {
       
