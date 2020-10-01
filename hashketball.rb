005f063e502f3d32ef6d58989c93d53edf2433bd
Inga <ijaanimets@gmail.com>
2020-10-01 21:59:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,14 @@
 # Write your code here!
 
 def game_hash
-  {}
+  player = 
+  team = { team_name: '', colors: ['red'], players: [player] }
+  {
+    :home => {
+      
+    },
+    :away => {
+      
+    }
+  }
 end
