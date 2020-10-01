0dfc4c95e3a846fa937c6b83da4f17c4eac79edf
Inga <ijaanimets@gmail.com>
2020-10-01 22:10:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -103,7 +103,7 @@ def game_hash
           :slam_dunks => 0
         },
         {
-          :player_name => 'Mason Plumlee',
+          :player_name => 'Ben Gordon',
           :number => 0,
           :shoe => 0,
           :points => 0,
